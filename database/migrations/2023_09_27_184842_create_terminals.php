fa420ea552d96ae242432892db00bd48ce80a366
gajn01 <garciajuanmiguel1105@gmail.com>
2023-10-05 05:52:57 +0800

    change terminal lat long from decimal to int

--- database/migrations/2023_09_27_184842_create_terminals.php
+++ database/migrations/2023_09_27_184842_create_terminals.php
@@ -18,8 +18,8 @@ return new class extends Migration
             $table->unsignedBigInteger('toda_id');
             $table->string('terminal_name');
             $table->text('terminal_address')->nullable();
-            $table->decimal('terminal_long', 10, 7)->nullable();
-            $table->decimal('terminal_lat', 10, 7)->nullable();
+            $table->integer('terminal_long')->nullable();
+            $table->integer('terminal_lat')->nullable();
             $table->timestamps(); // Adds created_at and updated_at columns
             $table->foreign('toda_id')->references('id')->on('todas');
         });
